2feeeb897e161fb2161ec1bae68221226c574396
tshepo <tshepo941028@gmail.com>
2021-12-28 21:20:27 +0200

    Initial Commit 2

--- Main Projects/App Brewery - Advanced/day69 blog-with-users-start/forms.py
+++ Main Projects/App Brewery - Advanced/day69 blog-with-users-start/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, EmailField
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 
@@ -14,14 +14,14 @@ class CreatePostForm(FlaskForm):
 
 
 class RegisterForm(FlaskForm):
-    email = EmailField('Email', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     name = StringField('Name', validators=[DataRequired()])
     register = SubmitField('Sign me Up!')
 
 
 class LoginForm(FlaskForm):
-    email = EmailField('Email', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     login = SubmitField('Let me In!')
 
